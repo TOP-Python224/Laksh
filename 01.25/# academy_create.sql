b44c5e67b6513636f9e902dc19a79a0909c1f035
Gennadiy <renardes@gmail.com>
2023-01-24 14:27:55 +0500

    домашнее задание от 01.25
    обновление SQL скриптов для создания и заполнения схемы Академия

--- 01.25/# academy_create.sql
+++ 01.25/# academy_create.sql
@@ -1,144 +1,156 @@
 drop database if exists academy;
 
-create database if not exists academy;
+create database academy;
 use academy;
 
-create table if not exists `Curators` (
-	`id` mediumint unsigned not null auto_increment,
-    `Name` varchar(30) not null,
-    `Surname` varchar(30) not null,
-    constraint `PK_id` primary key(`id`)
-);
-alter table `Curators`
-	add constraint `CH_Curators_Name` check (`Name` <> ''),
-    add constraint `CH_Curators_Surname` check (`Surname` <> '');
 
-create table if not exists `Departments` (
-	`id` smallint unsigned not null auto_increment,
-    `Building` tinyint not null,
-    `Financing` decimal(10,2) not null default 0,
-    `Name` varchar(100) not null unique,
-    `FacultyId` tinyint unsigned not null,
-    constraint `PK_id` primary key(`id`),
-    constraint `CH_Departments_Building` check (`Building` between 1 and 5),
-    constraint `CH_Departments_Financing` check (`Financing` >= 0),
-    constraint `CH_Departments_Name` check (`Name` <> '')
-);
-
-create table if not exists `Faculties` (
+create table `faculties` (
 	`id` tinyint unsigned not null auto_increment,
-	`Name` varchar(100) not null unique,
-    constraint `PK_id` primary key(`id`),
-    constraint `CH_Faculties_Name` check (`Name` <> '')
+    `financing`  decimal(10,2) not null default 0,
+	`name` varchar(100) not null unique,
+    constraint `PK_id` primary key (`id`),
+    constraint `CH_faculties_name` check (`name` <> '')
 );
 
-create table if not exists `Groups` (
-	`id` mediumint unsigned not null auto_increment,
-	`Name` varchar(10) not null unique,
-    `Year` tinyint not null,
-    `DepartmentId` smallint unsigned not null,
-    constraint `PK_id` primary key(`id`),
-    constraint `CH_Groups_Name` check (`Name` <> ''),
-    constraint `CH_Groups_Year` check (`Year` between 1 and 6)
-);
 
-create table if not exists `GroupsCurators` (
-	`id` int unsigned not null auto_increment,
-    `CuratorId` mediumint unsigned not null,
-    `GroupId` mediumint unsigned not null,
-    constraint `PK_id` primary key(`id`)
-);
-
-create table if not exists `GroupsLectures` (
-	`id` int unsigned not null auto_increment,
-    `LectureId` int unsigned not null,
-    `GroupId` mediumint unsigned not null,
-    constraint `PK_id` primary key(`id`)
-);
-
-create table if not exists `GroupsStudents` (
-	`id` int unsigned not null auto_increment,
-    `StudentId` mediumint unsigned not null,
-    `GroupId` mediumint unsigned not null,
-    constraint `PK_id` primary key(`id`)
-);
-
-create table if not exists `Lectures` (
-	`id` int unsigned not null auto_increment,
-    `Date` date not null,
-    `SubjectId` smallint unsigned not null,
-    `TeacherId` smallint unsigned not null,
-    constraint `PK_id` primary key(`id`)
-);
-
-create table if not exists `Students` (
-	`id` mediumint unsigned not null auto_increment,
-    `Name` varchar(30) not null,
-    `Surname` varchar(30) not null,
-    `Rating` tinyint not null,
-    constraint `PK_id` primary key(`id`),
-    constraint `CH_Students_Name` check (`Name` <> ''),
-    constraint `CH_Students_Surname` check (`Surname` <> ''),
-    constraint `CH_Students_Rating` check (`Rating` between 0 and 5)
-);
-
-create table if not exists `Subjects` (
+create table `departments` (
 	`id` smallint unsigned not null auto_increment,
-	`Name` varchar(100) not null unique,
-    constraint `PK_id` primary key(`id`),
-    constraint `CH_Subjects_Name` check (`Name` <> '')
+    `building` tinyint not null,
+    `financing` decimal(10,2) not null default 0,
+    `name` varchar(100) not null unique,
+    `faculty_id` tinyint unsigned not null,
+    constraint `PK_id` primary key (`id`),
+    constraint `CH_departments_building` check (`building` between 1 and 5),
+    constraint `CH_departments_financing` check (`financing` >= 0),
+    constraint `CH_departments_name` check (`name` <> '')
 );
 
-create table if not exists `Teachers` (
+
+create table `groups` (
+	`id` mediumint unsigned not null auto_increment,
+	`name` varchar(10) not null unique,
+    `year` tinyint not null,
+    `department_id` smallint unsigned not null,
+    constraint `PK_id` primary key (`id`),
+    constraint `CH_groups_name` check (`name` <> ''),
+    constraint `CH_groups_year` check (`year` between 1 and 6)
+);
+
+
+create table `curators` (
+	`id` mediumint unsigned not null auto_increment,
+    `name` varchar(30) not null,
+    `surname` varchar(30) not null,
+    constraint `PK_id` primary key (`id`),
+    constraint `CH_curators_name` check (`name` <> ''),
+    constraint `CH_curators_surname` check (`surname` <> '')
+);
+
+
+create table `groups_curators` (
+	`id` int unsigned not null auto_increment,
+    `curator_id` mediumint unsigned not null,
+    `group_id` mediumint unsigned not null,
+    constraint `PK_id` primary key (`id`)
+);
+
+
+create table `teachers` (
 	`id` smallint unsigned not null auto_increment,
-    `Name` varchar(30) not null,
-    `Surname` varchar(30) not null,
-    `Salary` decimal(8,2) not null,
-    `IsProfessor` bit(1) not null default 0,
-    constraint `PK_id` primary key(`id`),
-    constraint `CH_Teachers_Name` check (`Name` <> ''),
-    constraint `CH_Teachers_Surname` check (`Surname` <> ''),
-    constraint `CH_Teachers_Salary` check (`Salary` > 0)
+    `name` varchar(30) not null,
+    `surname` varchar(30) not null,
+    `salary` decimal(8,2) not null,
+    `is_professor` bit(1) not null default 0,
+    constraint `PK_id` primary key (`id`),
+    constraint `CH_teachers_name` check (`name` <> ''),
+    constraint `CH_teachers_surname` check (`surname` <> ''),
+    constraint `CH_teachers_salary` check (`salary` > 0)
 );
 
-alter table `Departments` 
-	add constraint `FK_Departments_FacultyId` 
-		foreign key(`FacultyId`) 
-        references `Faculties`(`id`);
 
-alter table `Groups` 
-	add constraint `FK_Groups_DepartmentId` 
-		foreign key(`DepartmentId`) 
-        references `Departments`(`id`);
+create table `subjects` (
+	`id` smallint unsigned not null auto_increment,
+	`name` varchar(100) not null unique,
+    constraint `PK_id` primary key (`id`),
+    constraint `CH_subjects_name` check (`name` <> '')
+);
 
-alter table `GroupsCurators` 
-	add constraint `FK_GroupsCurators_CuratorId` 
-        foreign key(`CuratorId`) 
-        references `Curators`(`id`),
-	add constraint `FK_GroupsCurators_GroupId` 
-        foreign key(`GroupId`) 
-        references `Groups`(`id`);
 
-alter table `GroupsLectures` 
-	add constraint `FK_GroupsLectures_LectureId` 
-        foreign key(`LectureId`) 
-        references `Lectures`(`id`),
-	add constraint `FK_GroupsLectures_GroupId` 
-        foreign key(`GroupId`) 
-        references `Groups`(`id`);
+create table `lectures` (
+	`id` int unsigned not null auto_increment,
+    `date` date not null,
+    `subject_id` smallint unsigned not null,
+    `teacher_id` smallint unsigned not null,
+    constraint `PK_id` primary key (`id`)
+);
 
-alter table `GroupsStudents` 
-	add constraint `FK_GroupsStudents_StudentId` 
-        foreign key(`StudentId`)
-        references `Students`(`id`),
-	add constraint `FK_GroupsStudents_GroupId` 
-        foreign key(`GroupId`) 
-        references `Groups`(`id`);
 
-alter table `Lectures` 
-	add constraint `FK_Lectures_SubjectId` 
-        foreign key(`SubjectId`) 
-        references `Subjects`(`id`),
-	add constraint `FK_Lectures_TeacherId` 
-        foreign key(`TeacherId`) 
-        references `Teachers`(`id`);
+create table `groups_lectures` (
+	`id` int unsigned not null auto_increment,
+    `lecture_id` int unsigned not null,
+    `group_id` mediumint unsigned not null,
+    constraint `PK_id` primary key (`id`)
+);
+
+
+create table `students` (
+	`id` mediumint unsigned not null auto_increment,
+    `name` varchar(30) not null,
+    `surname` varchar(30) not null,
+    `rating` tinyint not null,
+    constraint `PK_id` primary key (`id`),
+    constraint `CH_students_name` check (`name` <> ''),
+    constraint `CH_students_surname` check (`surname` <> ''),
+    constraint `CH_students_rating` check (`rating` between 0 and 5)
+);
+
+
+create table `groups_students` (
+	`id` int unsigned not null auto_increment,
+    `student_id` mediumint unsigned not null,
+    `group_id` mediumint unsigned not null,
+    constraint `PK_id` primary key (`id`)
+);
+
+
+alter table `departments` 
+	add constraint `FK_departments_faculty_id` 
+		foreign key (`faculty_id`) 
+        references `faculties` (`id`);
+
+alter table `groups` 
+	add constraint `FK_groups_department_id` 
+		foreign key (`department_id`) 
+        references `departments` (`id`);
+
+alter table `lectures` 
+	add constraint `FK_lectures_subject_id` 
+        foreign key (`subject_id`) 
+        references `subjects` (`id`),
+	add constraint `FK_lectures_teacher_id` 
+        foreign key (`teacher_id`) 
+        references `teachers` (`id`);
+
+alter table `groups_curators` 
+	add constraint `FK_groups_curators_curator_id` 
+        foreign key (`curator_id`) 
+        references `curators` (`id`),
+	add constraint `FK_groups_curators_group_id` 
+        foreign key (`group_id`) 
+        references `groups` (`id`);
+
+alter table `groups_lectures` 
+	add constraint `FK_groups_lectures_lecture_id` 
+        foreign key (`lecture_id`) 
+        references `lectures` (`id`),
+	add constraint `FK_groups_lectures_group_id` 
+        foreign key (`group_id`) 
+        references `groups` (`id`);
+
+alter table `groups_students` 
+	add constraint `FK_groups_students_student_id` 
+        foreign key (`student_id`)
+        references `students` (`id`),
+	add constraint `FK_groups_students_group_id` 
+        foreign key (`group_id`) 
+        references `groups` (`id`);
